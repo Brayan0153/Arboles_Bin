1517b3a5106986a8db3ec18a8402d321f66d1758
Semana_7 <koutaroox0@gmail.com>
2026-05-12 12:16:35 -0500

    Agregar archivos de proyecto.

---
+++ Arboles_Bin/ArbolBB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arboles_Bin
+{
+    internal class ArbolBB
+    {
+        public Nodo raiz;
+
+        public ArbolBB()
+        {
+            raiz = null;
+
+        }
+
+        public void insertar(int info)
+        {
+
+        }
+
+
+       
+    }
+
+}
---
+++ Arboles_Bin/Nodo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arboles_Bin
+{
+    internal class Nodo
+    {
+        public int dato;
+        public Nodo izquierda, derecha;
+
+        public Nodo(int dato, Nodo izquierda, Nodo derecha)
+        {
+            this.dato = dato;
+            this.izquierda = null;
+            this.derecha = null;
+        }
+
+    }
+}
+
+
